f2968a2ecb961ee01ba6b50b06565c448a5eba7b
Anasmahfoud <156627676+Anasmahfoud@users.noreply.github.com>
2024-02-06 20:20:54 +0100

    Mobile nav & add to cart
    added mobile nav and add to cart and buy buttons on products!

---
+++ main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+  }
+
+
